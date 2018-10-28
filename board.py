880146a2df648cec9db8fb767b668b14d9ee39d4
Matthew Budimir <matthewbudimir@gmail.com>
2018-10-28 16:36:22 +1030

    fixing checkwinner code

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 import sys
+from copy import deepcopy
 # import curses
 
 class Board(object):
@@ -6,9 +7,6 @@ class Board(object):
     def __init__(self):
         super(Board, self).__init__()
         self.board = [[0]*7 for i in range(6)]
-        for row in self.board:
-            print row
-        # print self.board
     def changeState(self,player,move,state):
         # If this col is not full
         if state[0][move] == 0:
@@ -50,7 +48,10 @@ class Board(object):
                         for yShift in [1,0,-1]:
                             if xShift == 0 and yShift == 0:
                                 continue
-                            run = self.findRun(state,token,4,x,y,xShift,yShift)
+                            # print("Start")
+                            run = False
+                            if state[x][y] == token:
+                                run = self.findRun(state,token,4,x,y,xShift,yShift)
                             if run:
                                 return token
         return 0
@@ -60,107 +61,12 @@ class Board(object):
         # Check if thing is in bounds (Faster than using 'in' with range... Maybe.)
         if 0 <= (x+xshift) < len(state) and 0 <= (y+yshift) < len(state[0]):
             if state[x+xshift][y+yshift] == token:
-                if count == 1:
+                if count == 2:
                     return True
+
                 else:
                     return self.findRun(state,token,count-1,x+xshift,y+yshift,xshift,yshift)
             else:
                 return False
         else:
             return False
-
-
-
-# board = Board()
-# board.printBoard()
-
-# player = 1
-# ai = MonteCarlo(2,board,-1);
-# while True:
-#     move = raw_input("MOVE:")
-#     if move.replace('-','',1).isdigit():
-#         move = int(move)
-#     else:
-#         print("Please enter a number")
-#         continue
-#     if not 0 <= move < 7:
-#         print("Not valid column. Try again")
-#         continue
-    
-#     board.setState(board.changeState(player,move,board.board))
-#     state = board.board
-    
-#     board.printBoard()
-#     if board.checkWinner(state) != 0:
-#         print("winner")
-#         print(1 if player else 2)
-#         exit()
-#     print(board.getMoves(1,board.board))
-#     player = player*-1
-        
-
-    
-
-# import curses
-# from curses import wrapper
-
-# import time
-
-# def main(stdscr):
-#     # Make stdscr.getch non-blocking
-#     stdscr.nodelay(True)
-#     stdscr.clear()
-#     width = 4
-#     count = 0
-#     direction = 1
-#     while True:
-#         c = stdscr.getch()
-#         # Clear out anything else the user has typed in
-#         curses.flushinp()
-#         stdscr.clear()
-#         # If the user presses p, increase the width of the springy bar
-#         if c == ord('p'):
-#             width += 1
-#         # Draw a springy bar
-#         stdscr.addstr("#" * count)
-#         count += direction
-#         if count == width:
-#             direction = -1
-#         elif count == 0:
-#             direction = 1
-#         # Wait 1/10 of a second. Read below to learn about how to avoid
-#         # problems with using time.sleep with getch!
-#         time.sleep(0.1)
-
-"""
-Calling stdscr.nodelay(True) made stdscr.getch() non-blocking. If Python gets to
-that line and the user hasn't typed anything since last time, getch will return
--1, which doesn't match any key.
-
-What if the user managed to type more than one character since the last time
-getch was called? All of those characters will start to build up, and getch will
-return the value for each one in the order that they came. This can cause
-delayed reactions if you're writing a game. After getch, you can call
-curses.flushinp to clear out the rest of the characters that the user typed.
-
-This is a good place to talk more about getch.
-
-Every time the user presses a key, that key's value gets stored in a list. When
-getch is called, it goes to that list and pops that value. If the user manages
-to press several keys before getch is called, getch will pop the least recently
-added value (the oldest key pressed). The rest of the keys remain in the list!
-The process continues like this. So there's a problem if there is a delay
-between calls to getch: Key values can build up. If you don't want this to
-happen, curses.flushinp() clears the list of inputted values. This ensures that
-the next key the user presses after curses.flushinp() is what getch will return
-next time it is called.
-"""
-
-"""
-To continue learning about curses, checkout the addstr method to see how you can
-print strings at certain y, x coordinates. You can start here:
-https://docs.python.org/3/library/curses.html#window-objects
-"""
-# wrapper(main)
-# curses.nocbreak(); stdscr.keypad(0); curses.echo()
-# curses.endwin()
